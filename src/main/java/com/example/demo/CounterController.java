6a7ad48281752544e2dba8c8e628f452c66ad919
tHirai67 <thirai67lmbj@gmail.com>
2023-04-27 03:03:30 +0900

    Updated the following: 1. Display of processing contents 2. Remove unnecessary

--- src/main/java/com/example/demo/CounterController.java
+++ src/main/java/com/example/demo/CounterController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -20,12 +21,14 @@ public class CounterController {
 	Integer money = 0;
 	
 	@GetMapping(value = "/home")
-	public String load(@ModelAttribute MerchandiseList ml, Model model) {
+	public String load(@ModelAttribute MerchandiseList ml, Model model, @ModelAttribute("status")String status) {
 		if(money == 0) {
-			model.addAttribute("m1", "所持金を入力してください");
-		}else{
-			model.addAttribute("m2", "所持金は"+money+"円です");
+			model.addAttribute("money", "所持金を入力してください");
+		}else {
+			model.addAttribute("money", "所持金:" + money + "円です");
 		}
+		//受信した処理内容を表示
+		model.addAttribute("status", status);
 	
 		Integer sumPrice = 0;
 		//listに格納されていたら表示
@@ -35,51 +38,46 @@ public class CounterController {
 			for(Merchandise merchandise : list ) {
 				 sumPrice += merchandise.getPrice(); 
 			}
+			model.addAttribute("sum_price","合計金額は" + sumPrice + "円です！");
 		}
-		model.addAttribute("m4","合計金額は" + sumPrice + "円です！");
+		
 		
 		
 		return "home";
 	}
 	
 	@PostMapping(value = "/home/edit", params="money_edit")
-	public String editMoney(@ModelAttribute MerchandiseList ml, Model model, @RequestParam(name = "money", required=false)Integer m) {
+	public String editMoney(RedirectAttributes ra, @RequestParam(name = "money", required=false)Integer m) {
 		//所持金の表示
-		if(money != null) {
-			money = m;
-		}
+		money = m;
+		//処理内容を送信
+		ra.addFlashAttribute("status","所持金が"+m+"円に更新されました");
 		
 		return "redirect:/home";
 	}
 	
 	//追加ボタンを押下　→　新しい入力項目を表示する
 	@PostMapping(value = "/home/edit", params="add")//paramsはbuttonタグのname属性
-	public String addList(@ModelAttribute MerchandiseList ml, Model model,
-			@RequestParam("m_name")String name,@RequestParam("m_price")Integer price ) {
+	public String addList(@ModelAttribute Merchandise merchandise, RedirectAttributes ra) {
 		
-		//入力をistに追加
-		Merchandise merchandise = new Merchandise();
-		merchandise.setName(name);
-		merchandise.setPrice(price);
+		//入力をlistに追加
 		list.add(merchandise);
-		model.addAttribute("m3", name+"("+price+")"+"が追加されました．");
+		//処理内容を送信
+		ra.addAttribute("status", merchandise.getName()+"("+merchandise.getPrice()+"円)"+"が追加されました．");
 		return "redirect:/home";
 	}
 	
 	//削除ボタンを押下
 	@PostMapping(value = "/home/edit", params="remove")
-	public String removeList(@ModelAttribute MerchandiseList ml, Model model, HttpServletRequest request) {
+	public String removeList(RedirectAttributes ra, HttpServletRequest request) {
 		//削除ボタンを押した行番号を取得
 		int index = Integer.valueOf(request.getParameter("remove"));
+		//処理内容を送信
+		ra.addAttribute("status", list.get(index).getName()+"("+list.get(index).getPrice()+"円)"+"が削除されました．");
 		//削除
 		list.remove(index);
+		
 		return "redirect:/home";
 	}
-	
-	
-	
-	
-	
-	
 	
 }
